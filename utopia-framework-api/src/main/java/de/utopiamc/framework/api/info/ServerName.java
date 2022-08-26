e9ad459edf8f7f13ad26d9d20a06f37a3223e956
OskarWiedeweg <oskarwiedeweg01@gmail.com>
2022-08-26 13:41:37 +0200

    Added cloudnet name constants

---
+++ utopia-framework-api/src/main/java/de/utopiamc/framework/api/info/ServerName.java
@@ -0,0 +1,14 @@
+package de.utopiamc.framework.api.info;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+public @interface ServerName {
+
+}
