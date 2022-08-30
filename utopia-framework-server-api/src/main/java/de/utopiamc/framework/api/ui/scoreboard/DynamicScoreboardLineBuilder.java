901df76392d48ace3d90f6510b2e6b395f15a20d
OskarWiedeweg <oskarwiedeweg01@gmail.com>
2022-08-30 12:39:00 +0200

    finished dynamic scoreboard lines

--- utopia-framework-server-api/src/main/java/de/utopiamc/framework/api/ui/scoreboard/DynamicScoreboardLineBuilder.java
+++ utopia-framework-server-api/src/main/java/de/utopiamc/framework/api/ui/scoreboard/DynamicScoreboardLineBuilder.java
@@ -1,16 +1,11 @@
 package de.utopiamc.framework.api.ui.scoreboard;
 
-import de.utopiamc.framework.api.model.VariableEventHandler;
-
-import java.util.function.Supplier;
-
 public interface DynamicScoreboardLineBuilder extends ScoreboardLineBuilder {
 
     DynamicScoreboardLineBuilder setTitle(String title);
+    DynamicScoreboardLineBuilder setContent(String title);
 
-    DynamicScoreboardLineBuilder setContent(String content);
-
-    <E> DynamicScoreboardLineBuilder addDynamicVariable(String name, String defaultValue, Class<E> event, VariableEventHandler<E> handler);
-    <E> DynamicScoreboardLineBuilder addDynamicVariable(String name, Supplier<String> defaultValue, Class<E> event, VariableEventHandler<E> handler);
+    ScoreboardLineUpdater setTitle();
+    ScoreboardLineUpdater setContent();
 
 }
